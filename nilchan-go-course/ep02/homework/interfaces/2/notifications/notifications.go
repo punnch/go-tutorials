b52bc0d65154513cf2cb15d5c0e4cc2ed926ce30
Punnch <punnchxd@gmail.com>
2025-12-24 20:43:14 +0300

    Update comments in 'homework/interfaces/notificaions.go'

--- nilchan-go-course/ep02/homework/interfaces/2/notifications/notifications.go
+++ nilchan-go-course/ep02/homework/interfaces/2/notifications/notifications.go
@@ -1,40 +1,28 @@
-// package notifications
-// To improve iteraction with notifications
 package notifications
 
-// Notifier interface
-// Stores notify methods
-// (e.g. push, sms, email)
+// Notifier interface stores notify methods (e.g. push, sms, email)
 type Notifier interface {
 	Send(to, message string)
 }
 
-// NotificationModule struct
-// Stores a map
-// - key is a name that user did for his message
-// - value is a notify method of sending a message
+// NotificationModule struct stores a map for different channels
 type NotificationModule struct {
 	notifiers map[string]Notifier
 }
 
-// NewNotifierModule()
-// Used for encapsulation
-// Returns a *NotificationModule with the created map field
+// NewNotifierModul creates a new NotificationModule with an empty Notifier map
 func NewNotifierModule() *NotificationModule {
 	return &NotificationModule{
 		notifiers: make(map[string]Notifier),
 	}
 }
 
-// AddNotifier()
-// Adds a notifier in the map with an interface value
+// AddNotifier adds a Notifier with the given name to the map
 func (n *NotificationModule) AddNotifier(name string, notifier Notifier) {
 	n.notifiers[name] = notifier
 }
 
-// SendAll()
-// Recieves a notification map created by user
-// Checks if it has a send method then uses a Send() function
+// SendAll sends all the messages of a recieved map
 func (n *NotificationModule) SendAll(notifications map[string]string) {
 	for channel, message := range notifications {
 		notifier, ok := n.notifiers[channel]
