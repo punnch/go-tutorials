00023b9dea2c263971831ad3f256dff509131ccc
punnch <punnchxd@gmail.com>
2026-01-05 09:37:43 +0300

    fix: correct logic in homework task

--- nilchan-course/ep02/homework/waitgr/main.go
+++ nilchan-course/ep02/homework/waitgr/main.go
@@ -7,29 +7,30 @@ import (
 	"time"
 )
 
-func gardener(wg *sync.WaitGroup, area string) {
+func gardener(wg *sync.WaitGroup, n int) {
 	defer wg.Done()
 
-	fmt.Println("Я огородник", rand.Intn(5)+1, "и удобряю", area)
+	fmt.Println("Я огородник", n, "начинаю полив...")
 	time.Sleep(time.Duration(500+rand.Intn(501)) * time.Millisecond)
+	fmt.Println("Я огородник", n, "полив закончил!")
 }
 
 func main() {
+	min := 3
+	max := 10
+
+	// range: 3-10
+	gardeners := min + rand.Intn(max-min+1)
+
 	wg := &sync.WaitGroup{}
 
-	// 1
-	wg.Add(1)
-	go gardener(wg, "лилии")
-
-	// 2
-	wg.Add(1)
-	go gardener(wg, "розы")
-
-	// 3
-	wg.Add(1)
-	go gardener(wg, "ромашки")
+	for i := 1; i <= gardeners; i++ {
+		wg.Add(1)
+		go gardener(wg, i)
+	}
 
 	wg.Wait()
 
+	fmt.Println("-----------------------------")
 	fmt.Println("Огородники закончили свою работу!")
 }
