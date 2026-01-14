b390d327c13f27199040d98a6d857aa15c0bad13
punnch <punnchxd@gmail.com>
2026-01-14 20:57:39 +0300

    feat: add respbody homework task 2

---
+++ nilchan-course/ep02/homework/respbody/2/main.go
@@ -0,0 +1,172 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+	"sync"
+)
+
+var (
+	messages map[int]string
+	id       int
+	mtx      sync.Mutex
+)
+
+func idHandler(w http.ResponseWriter, r *http.Request) {
+	httpRequestBody, err := io.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+
+		msg := "fail http server error:" + err.Error()
+		fmt.Println(msg)
+
+		if _, err := w.Write([]byte(msg)); err != nil {
+			fmt.Println("fail http server error:", err)
+		}
+	}
+
+	idMessage, err := strconv.Atoi(string(httpRequestBody))
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+
+		msg := "fail to convert http request body to int:" + err.Error()
+		fmt.Println(msg)
+
+		if _, err := w.Write([]byte(msg)); err != nil {
+			fmt.Println("fail http server error:", err)
+		}
+		return
+	}
+
+	mtx.Lock()
+	_, ok := messages[idMessage]
+	if ok {
+		msg := messages[idMessage]
+		fmt.Println(msg)
+
+		if _, err := w.Write([]byte(msg)); err != nil {
+			fmt.Println("fail http server error:", err)
+		}
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+
+		msg := "fail to show, no message with provided id"
+		fmt.Println(msg)
+
+		if _, err := w.Write([]byte(msg)); err != nil {
+			fmt.Println("fail http server error:", err)
+		}
+	}
+	mtx.Unlock()
+}
+
+func listHandler(w http.ResponseWriter, r *http.Request) {
+	var str string
+
+	mtx.Lock()
+	for k, v := range messages {
+		str += fmt.Sprintf("\n%d - %s", k, v)
+	}
+	mtx.Unlock()
+
+	msg := "Messages:" + str
+	fmt.Println(msg)
+
+	if _, err := w.Write([]byte(msg)); err != nil {
+		fmt.Println("fail http server error:", err)
+	}
+}
+
+func messageHandler(w http.ResponseWriter, r *http.Request) {
+	httpRequestBody, err := io.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+
+		msg := "fail http server error:" + err.Error()
+		fmt.Println(msg)
+
+		if _, err := w.Write([]byte(msg)); err != nil {
+			fmt.Println("fail http server error:", err)
+		}
+		return
+	}
+
+	message := string(httpRequestBody)
+
+	mtx.Lock()
+	id++
+	messages[id] = message
+	mtx.Unlock()
+
+	msg := "Add:" + strconv.Itoa(id) + " - " + message
+	fmt.Println(msg)
+
+	if _, err := w.Write([]byte(msg)); err != nil {
+		fmt.Println("fail http server error:", err)
+	}
+}
+
+func deleteHandler(w http.ResponseWriter, r *http.Request) {
+	httpRequestBody, err := io.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+
+		msg := "fail http server error:" + err.Error()
+		fmt.Println(msg)
+
+		if _, err := w.Write([]byte(msg)); err != nil {
+			fmt.Println("fail http server error:", err)
+		}
+		return
+	}
+
+	idMessage, err := strconv.Atoi(string(httpRequestBody))
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+
+		msg := "fail to convert http request body to int:" + err.Error()
+		fmt.Println(msg)
+
+		if _, err := w.Write([]byte(msg)); err != nil {
+			fmt.Println("fail http server error:", err)
+		}
+		return
+	}
+
+	mtx.Lock()
+	_, ok := messages[idMessage]
+	if ok {
+		msg := "Delete:" + strconv.Itoa(idMessage) + " - " + messages[idMessage]
+		fmt.Println(msg)
+
+		if _, err := w.Write([]byte(msg)); err != nil {
+			fmt.Println("fail http server error:", err)
+		}
+		delete(messages, idMessage)
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+
+		msg := "fail to delete, no message with provided id"
+		fmt.Println(msg)
+
+		if _, err := w.Write([]byte(msg)); err != nil {
+			fmt.Println("fail http server error:", err)
+		}
+	}
+	mtx.Unlock()
+}
+
+func main() {
+	messages = make(map[int]string)
+
+	http.HandleFunc("/id", idHandler)
+	http.HandleFunc("/list", listHandler)
+	http.HandleFunc("/message", messageHandler)
+	http.HandleFunc("/delete", deleteHandler)
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("fail server error:", err)
+	}
+}
