db974b56a7bb5771d5d27581b03db049c76fb5d3
punnch <punnchxd@gmail.com>
2026-01-20 19:46:14 +0300

    feat: add gorilla/mux library

--- nilchan-course/ep02/lesson/restapi/http/server.go
+++ nilchan-course/ep02/lesson/restapi/http/server.go
@@ -1,3 +1,32 @@
 package http
 
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 // main goal: what handler should I execute in that moment if... (interaction with handlers)
+
+type HTTPServer struct {
+	httpHandlers *HTTPHandlers
+}
+
+func NewHTTPServer(httpHandlers *HTTPHandlers) *HTTPServer {
+	return &HTTPServer{
+		httpHandlers: httpHandlers,
+	}
+}
+
+func (s *HTTPServer) StartServer() error {
+	router := mux.NewRouter()
+
+	router.Path("/tasks").Methods("POST").HandlerFunc(s.httpHandlers.HandleCreateTask)
+	router.Path("/tasks/{title}").Methods("GET").HandlerFunc(s.httpHandlers.HandleGetTask)
+	router.Path("/tasks").Methods("GET").HandlerFunc(s.httpHandlers.HandleGetAllTasks)
+	router.Path("/tasks").Methods("GET").Queries("completed", "false").HandlerFunc(s.httpHandlers.HandleGetAllNotCompletedTasks)
+	router.Path("/tasks/{title}").Methods("PATCH").HandlerFunc(s.httpHandlers.HandleCompleteTask)
+	router.Path("/tasks/{title}").Methods("DELETE").HandlerFunc(s.httpHandlers.HandleDeleteTask)
+
+	return http.ListenAndServe(":9091", router)
+}
