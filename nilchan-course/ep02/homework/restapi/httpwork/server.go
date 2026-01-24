dbc07a08476c498a86616a4efc8abbd1e5502610
punnch <punnchxd@gmail.com>
2026-01-24 21:16:04 +0300

    feat(httpwork): add routing

--- nilchan-course/ep02/homework/restapi/httpwork/server.go
+++ nilchan-course/ep02/homework/restapi/httpwork/server.go
@@ -1,5 +1,12 @@
 package httpwork
 
+import (
+	"errors"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 type HTTPServer struct {
 	httpHandlers *HTTPHandlers
 }
@@ -10,6 +17,20 @@ func NewHttpServer(httpHandlers *HTTPHandlers) *HTTPServer {
 	}
 }
 
-func (s *HTTPServer) Start() {
+func (s *HTTPServer) Start() error {
+	router := mux.NewRouter()
 
+	router.Path("/books").Methods("POST").HandlerFunc(s.httpHandlers.HandleCreateBook)
+	router.Path("/books/{title}").Methods("PATCH").HandlerFunc(s.httpHandlers.HandleReadBook)
+	router.Path("/books/{title}").Methods("GET").HandlerFunc(s.httpHandlers.HandleGetBook)
+	router.Path("/books").Methods("GET").HandlerFunc(s.httpHandlers.HandleGetBooks)
+	router.Path("/books/{title}").Methods("DELETE").HandlerFunc(s.httpHandlers.HandleDeleteBook)
+
+	if err := http.ListenAndServe(":9091", router); err != nil {
+		if errors.Is(err, http.ErrServerClosed) {
+			return nil
+		}
+		return err
+	}
+	return nil
 }
